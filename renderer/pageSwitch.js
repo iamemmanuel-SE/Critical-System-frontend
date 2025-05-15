64b1b5ca10c450a9f4d98249c79655832ba8f416
Emmanuel Amankwah <emma@Emmanuels-MacBook-Pro.local>
2025-05-15 06:05:06 +0100

    Dash boardToggel done

--- renderer/pageSwitch.js
+++ renderer/pageSwitch.js
@@ -7,29 +7,72 @@ document.addEventListener("DOMContentLoaded", () => {
   const dashboardWrapperStaff = document.getElementById("dashboard-wrapperStaff");
   const otpPage = document.getElementById("OtpPage");
 
-   
+
+
   //User DashBoard tabs================================================
   const userCreatAccTab = document.getElementById("userCreatAccTab");
-  const transactionHisTab = document.getElementsById("transactionHisTab");
-  const withdrawTab = document.getElementById("withdrawTab");
-  const AcountDetails = document.getElementById("AcountDetails");
+  const userTransactionHisTab = document.getElementById("transactionHisTab");
+  const userWithdrawTab = document.getElementById("withdrawTab");
+  const userAcountDetailsTab = document.getElementById("AcountDetails");
 
   //User DashBoard Toggle Buttons=============================================
-  const createAccountButton = document.getElementById("createAccountButton")
-  const transactionButton = document.getElementById("transactionButton ")
-  const widthdrawButton = document.getElementById ("transactionButton ")
-  const  accountDetailsButtons = document.getElementById ("transactionButton ")
+  const userCreateAccountButton = document.getElementById("createAccountButton");
+  const userTransactionButton = document.getElementById("transactionButton");
+  const userWithdrawButton = document.getElementById ("widthdrawButton");
+  const userAccountDetailsButtons = document.getElementById ("accountDetailsButtons");
 
-  //Add toggle State function for User Accounts
-  function creditUserAccounts(){
-    if (userCreatAccTab) userCreatAccTab.style.display = "flex";
-    if (transactionHisTab) transactionHisTab.style.display = "none";
-    if (withdrawTab ) withdrawTab.style.display = "none";
-    if (AcountDetails ) AcountDetails .style.display = "none";
-    }
+  //Add the toggle fuctions for USER===============================================
+  //User create Account Func
+  function userCreatAccTabFunc(){
+    if(userCreatAccTab) userCreatAccTab.style.display = "flex";
+    if(userTransactionHisTab) userTransactionHisTab.style.display = "none"
+    if(userWithdrawTab) userWithdrawTab.style.display = "none"
+    if(userAcountDetailsTab) userAcountDetailsTab.style.display = "none"
+  }
 
+  //User Trasaction history Func
+  function userTransactionHisTabFunc(){
+    if(userCreatAccTab) userCreatAccTab.style.display = "none";
+    if(userTransactionHisTab) userTransactionHisTab.style.display = "flex"
+    if(userWithdrawTab) userWithdrawTab.style.display = "none"
+    if(userAcountDetailsTab) userAcountDetailsTab.style.display = "none"
+  }
+ 
+  //User Withdrawal Func
+  function userWithdrawTabFunc(){
+    if(userCreatAccTab) userCreatAccTab.style.display = "none";
+    if(userTransactionHisTab) userTransactionHisTab.style.display = "none"
+    if(userWithdrawTab) userWithdrawTab.style.display = "flex"
+    if(userAcountDetailsTab) userAcountDetailsTab.style.display = "none";
+  }
 
+  function userAcountDetailsTabFun(){
+    if(userCreatAccTab) userCreatAccTab.style.display = "none";
+    if(userTransactionHisTab) userTransactionHisTab.style.display = "none";
+    if(userWithdrawTab) userWithdrawTab.style.display = "none";
+    if(userAcountDetailsTab) userAcountDetailsTab.style.display = "flex";
+  }
 
+  //Add events to User Buttons=================================================
+  userCreateAccountButton.addEventListener('click', (e)=>{
+    e.preventDefault();
+      userCreatAccTabFunc();
+  })
+
+  userTransactionButton.addEventListener('click', (e)=>{
+  e.preventDefault();
+    userTransactionHisTabFunc();
+  })
+
+  userWithdrawButton.addEventListener('click', (e)=>{
+  e.preventDefault();
+  userWithdrawTabFunc();
+  })
+
+  userAccountDetailsButtons.addEventListener('click', (e)=>{
+  e.preventDefault();
+  userAcountDetailsTabFun();
+  })
   // Role Divider===========================================================================================================
   // Role Divider===========================================================================================================
   // Role Divider===========================================================================================================
@@ -41,27 +84,32 @@ document.addEventListener("DOMContentLoaded", () => {
   const withdrawForUserTab = document.getElementById("withdrawForUserTab")
 
 
+  //Staff DashBoard Toggle Buttons
+  const creditAccountButton = document.getElementById("creditAccountButton");
+  const viewAccountButton = document.getElementById("viewAccountButton");
+  const withdrawButton = document.getElementById("withdrawButton");
 
 
-  //Add toggle State function for staff Accounts
+
+  //Add toggle State function for User Accounts
   function creditUserAccounts(){
   if (creditAccTab) creditAccTab.style.display = "flex";
   if (viewAccountsTab) viewAccountsTab.style.display = "none";
-  if (withdrawForUserTab ) withdrawForUserTab .style.display = "none";
+  if (withdrawForUserTab) withdrawForUserTab .style.display = "none";
   }
 
-  //Add toggle State function for staff Accounts
+  //Add toggle State function for User Accounts
   function showUserAccounts(){
     if (creditAccTab) creditAccTab.style.display = "none";
     if (viewAccountsTab) viewAccountsTab.style.display = "flex";
-    if (withdrawForUserTab ) withdrawForUserTab .style.display = "none";
+    if (withdrawForUserTab) withdrawForUserTab.style.display = "none";
     }
 
-     //Add toggle State funtions for staff Accounts
+     //Add toggle State funtions for User Accounts
     function withdrawForUserTabFun(){
       if (creditAccTab) creditAccTab.style.display = "none";
       if (viewAccountsTab) viewAccountsTab.style.display = "none";
-      if (withdrawForUserTab ) withdrawForUserTab .style.display = "flex";
+      if (withdrawForUserTab) withdrawForUserTab.style.display = "flex";
       }
     
     
@@ -84,9 +132,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   })
   
-
-
-
 
   const toSignupTriggers = Array.from(document.getElementsByClassName("to-signup"));
   const toLoginTriggers = Array.from(document.getElementsByClassName("to-login"));
